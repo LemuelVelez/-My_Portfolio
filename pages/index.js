77fc5ac85fae142b375561e619404b702ab2d8bc
Lemuel Tumala Velez <138591940+LemuelVelez@users.noreply.github.com>
2023-10-27 16:04:58 +0800

    add text, title, and subtitle with classname styles

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,25 @@ import { motion } from 'framer-motion';
 import { fadeIn } from '../variants';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className='bg-primary/60 h-full'>
+      {/* text */}
+      <div className='w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10'>
+        <div className='text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto'>
+          {/* title */}
+          <h1 className='h1'>
+            Transforming Ideas <br /> Into {' '} <span className='text-accent'> Digital Reality</span>
+          </h1>
+          {/* subtitle */}
+          <p className='max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16'>
+            Curabitur aliquam erat eu tellus tristique iaculis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Ut euismod neque purus, vel volutpat nisl ornare.
+          </p>
+        </div>
+      </div>
+      {/* image */}
+      <div>image</div>
+    </div>
+  );
 };
 
 export default Home;
