04bb5d7d7196cb602cd04cc6b9661e3ffdda555d
Lemuel Tumala Velez <138591940+LemuelVelez@users.noreply.github.com>
2023-10-27 17:03:17 +0800

    update index.js and ProjectBtn.js

--- components/ProjectsBtn.js
+++ components/ProjectsBtn.js
@@ -8,7 +8,22 @@ import Link from 'next/link';
 import { HiArrowRight } from "react-icons/hi2";
 
 const ProjectsBtn = () => {
-  return <div>Project Btn</div>;
+  return (
+    <div className='mx-auto xl:mx-0'>
+      <Link href={'/work'}>
+        <div className='relative w-[155px] h-[155px] flex justify-center items-center bg-circleStar bg-cover bg-center bg-no-repeat group'>
+          <Image
+            src={'/rounded-text.png'}
+            width={121}
+            height={128}
+            alt=''
+            className='animate-spin-slow w-full h-full max-w-[121px] max-h-[128px]'
+          />
+          <HiArrowRight className='absolute text-4xl group-hover:translate-x-2 transition-all duration-300' />
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default ProjectsBtn;
--- pages/index.js
+++ pages/index.js
@@ -19,17 +19,35 @@ const Home = () => {
       <div className='w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10'>
         <div className='text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto'>
           {/* title */}
-          <h1 className='h1'>
+          <motion.h1
+            variants={fadeIn('down', 0.2)}
+            initial='hidden'
+            animate='show'
+            exit='hidden'
+            className='h1'>
             Transforming Ideas <br /> Into {' '} <span className='text-accent'> Digital Reality</span>
-          </h1>
+          </motion.h1>
           {/* subtitle */}
-          <p className='max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16'>
+          <motion.p
+            variants={fadeIn('down', 0.3)}
+            initial='hidden'
+            animate='show'
+            exit='hidden'
+            className='max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16'>
             Curabitur aliquam erat eu tellus tristique iaculis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Ut euismod neque purus, vel volutpat nisl ornare.
-          </p>
+          </motion.p>
           {/* btn */}
           <div className='flex justify-center xl:hidden'>
             <ProjectsBtn />
           </div>
+          <motion.div
+            variants={fadeIn('down', 0.4)}
+            initial='hidden'
+            animate='show'
+            exit='hidden'
+            className='hidden xl:flex'>
+            <ProjectsBtn />
+          </motion.div>
         </div>
       </div>
       {/* image */}
