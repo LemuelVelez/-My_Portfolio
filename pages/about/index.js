8cee1227e8cf2657d3bd20067e5a59bbd8da008e
Lemuel Tumala Velez <138591940+LemuelVelez@users.noreply.github.com>
2023-11-01 04:55:21 +0800

    make some adjustments

--- pages/about/index.js
+++ pages/about/index.js
@@ -98,6 +98,9 @@ import Circles from '../../components/Circles';
 import { motion } from 'framer-motion';
 import { fadeIn } from '../../variants';
 
+// counter
+import CountUp from "react-countup";
+
 const About = () => {
   const [index, setIndex] = useState(0);
   console.log(index);
@@ -110,11 +113,20 @@ const About = () => {
         initial='hidden'
         animate='show'
         exit='hidden'
-        className='hidden xl:flex absolute bottom-0 -left-[370px]'>
+        className='hidden xl:flex absolute bottom-0 -left-[320px]'>
         <Avatar />
       </motion.div>
       <div className='container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6'>
-        <div className='flex-1 flex flex-col justify-center'>text</div>
+        {/* text */}
+        <div className='flex-1 flex flex-col justify-center'>
+          <h2 className='h2'>
+            Captivating <span className='text-accent'>stories</span> birth magnificent designs.
+          </h2>
+          <p className='max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0'>
+            10 years ago, I began freelancing as a developer. Since then, I've done remote work for agencies, consulted for startups, and collaborated on digital products for business and consumer use.
+          </p>
+        </div>
+        {/* info */}
         <div className='flex flex-col w-full xl:max-w-[48%] h-[480px]'>
           <div className='flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4'>
             {aboutData.map((item, itemIndex) => {
@@ -133,9 +145,9 @@ const About = () => {
           <div className='py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start'>
             {aboutData[index].info.map((item, itemIndex) => {
               return (
-                <div 
-                key={itemIndex}
-                className='flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60'
+                <div
+                  key={itemIndex}
+                  className='flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60'
                 >
                   {/* title */}
                   <div className='font-light mb-2 md:mb-0'>{item.title}</div>
@@ -143,12 +155,12 @@ const About = () => {
                   <div>{item.stage}</div>
                   <div className='flex gap-x-4'>
                     {/* icons */}
-                    {item.icons?.map((icon, itemIndex)=>{
+                    {item.icons?.map((icon, itemIndex) => {
                       return <div className='text-2xl text-white'>{icon}</div>;
                     })}
                   </div>
                 </div>
-                );
+              );
             })}
           </div>
         </div>
