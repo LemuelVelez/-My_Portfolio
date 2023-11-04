47ef8f2d9e3e23d9f774c54feabf95ec679026b8
Lemuel Tumala Velez <138591940+LemuelVelez@users.noreply.github.com>
2023-11-04 22:04:38 +0800

    add bulb

--- pages/services/index.js
+++ pages/services/index.js
@@ -1,5 +1,57 @@
+// icons
+import {
+  RxCrop,
+  RxPencil2,
+  RxDesktop,
+  RxReader,
+  RxRocket,
+} from 'react-icons/rx';
+
+// service data
+export const serviceData = [
+  {
+    icon: <RxCrop />,
+    title: 'Branding',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  },
+  {
+    icon: <RxPencil2 />,
+    title: 'Design',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  },
+  {
+    icon: <RxDesktop />,
+    title: 'Development',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+  },
+  {
+    icon: <RxReader />,
+    title: 'Copywriting',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+  },
+  {
+    icon: <RxRocket />,
+    title: 'SEO',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+  },
+];
+
+// components
+import ServiceSlider from '../../components/ServiceSlider';
+import Bulb from '../../components/Bulb';
+import Circles from '../../components/Circles';
+
+// framer motion
+import { motion } from 'framer-motion';
+import { fadeIn } from '../../variants';
+
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <div className='h-full bg-primary/30 py-36 flex items-center'>
+      <Circles />
+      <div className='container mx-auto'></div>
+      <Bulb />
+    </div>);
 };
 
 export default Services;
