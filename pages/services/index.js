b5ecd24b5d089f66891f08c15d8cc957d95f75ef
Lemuel Tumala Velez <138591940+LemuelVelez@users.noreply.github.com>
2023-11-04 23:02:17 +0800

    change avatar, add swiper, add motion to services

--- pages/services/index.js
+++ pages/services/index.js
@@ -15,15 +15,35 @@ const Services = () => {
         <div className='flex flex-col xl:flex-row gap-x-8'>
           {/* text */}
           <div className='text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0'>
-            <h2 className='h2 xl:mt-8'>
+            <motion.h2
+              variants={fadeIn('up', 0.2)}
+              initial='hidden'
+              animate='show'
+              exit='hidden'
+              className='h2 xl:mt-8'>
               My Services <span className='text-accent'>.</span>
-            </h2>
-            <p>
+            </motion.h2>
+            <motion.p
+              variants={fadeIn('up', 0.4)}
+              initial='hidden'
+              animate='show'
+              exit='hidden'
+              className='mb-4 max-w-[400px] mx-auto lg:mx-0'
+            >
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla cursus, felis nec porttitor mattis, massa ipsum sollicitudin massa, nec pretium justo metus eget metus. Nunc auctor id eros ac faucibus.
-            </p>
+            </motion.p>
           </div>
+
           {/* slider */}
-          <ServiceSlider />
+          <motion.div
+            variants={fadeIn('down', 0.6)}
+            initial='hidden'
+            animate='show'
+            exit='hidden'
+            className='w-full xl:max-w-[65%]'
+          >
+            <ServiceSlider />
+          </motion.div>
         </div>
       </div>
       <Bulb />
